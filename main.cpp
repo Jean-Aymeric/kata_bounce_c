b69afc2f4165585efe51ba47c2423fb24ee27db0
Jean-Aymeric <jeanaymeric@gmail.com>
2024-09-20 10:19:02 +0200

    in progress

--- main.cpp
+++ main.cpp
@@ -1,21 +1,30 @@
 #include <iostream>
+
 #define FIELD_WIDTH 100
 #define BALL_SPRITE 'O'
-#define EMPTY_SPRITE ' '
+#define EMPTY_SPRITE '.'
+#define EDGE_SPRITE '|'
+#define FIELD_HAS_EDGES true
+#define BALL_STARTING_POSITION 0 + (FIELD_HAS_EDGES ? 1 : 0)
+#define BALL_STARTING_DIRECTION_RIGHT true
 
 int main() {
     char field[FIELD_WIDTH];
-    bool isBallMovingRight = true;
+    bool isBallMovingRight = BALL_STARTING_DIRECTION_RIGHT;
 
-    for (char &sprite : field) {
+    for (char &sprite: field) {
         sprite = EMPTY_SPRITE;
     }
-    field[0] = BALL_SPRITE;
+    if (FIELD_HAS_EDGES) {
+        field[0] = EDGE_SPRITE;
+        field[FIELD_WIDTH - 1] = EDGE_SPRITE;
+    }
+    field[BALL_STARTING_POSITION] = BALL_SPRITE;
 
-    for(;;) {
+    for (;;) {
         int fieldPosition = 0;
 
-        for (char sprite : field) {
+        for (char sprite: field) {
             std::cout << sprite;
         }
         std::cout << std::endl;
@@ -29,7 +38,7 @@ int main() {
                 field[fieldPosition] = EMPTY_SPRITE;
                 field[fieldPosition + 1] = BALL_SPRITE;
                 isBallMovingRight = true;
-            } else if (fieldPosition == FIELD_WIDTH - 1) {
+            } else if (fieldPosition == FIELD_HAS_EDGES ? FIELD_WIDTH - 2 : FIELD_WIDTH - 1) {
                 field[fieldPosition] = EMPTY_SPRITE;
                 field[fieldPosition - 1] = BALL_SPRITE;
                 isBallMovingRight = false;
